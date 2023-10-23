794b28455d27644729cad014f80385ab3ac9ba83
肥鸽子 <1214907360@qq.com>
2023-10-23 13:27:10 +0800

    first

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express(); //创建对象 express app下可以调用express的方法
+
+
+//路由  当接收到http的get方法
+app.get("/",(req,res) => {
+  res.status(200).json({
+      message:"hello from the back",
+      app:"fatdove"
+    })  //返回json对象
+})
+
+app.post("/",(req,res)=>{
+  res.send("you can post to this endpoint...")
+})
+
+
+//方便监听
+const port = 3000;
+app.listen(port,()=>{
+  console.log(`app running on port ${port}...`);
+})
